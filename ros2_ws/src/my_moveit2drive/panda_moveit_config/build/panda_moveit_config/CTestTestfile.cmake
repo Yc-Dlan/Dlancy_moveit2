816c412238599dbea4348fca4d52259ee9304281
Yc-Dlan <1940275781@qq.com>
2025-02-12 22:05:08 +0800

    构建ing

---
+++ ros2_ws/src/my_moveit2drive/panda_moveit_config/build/panda_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yc-dlan/Dlancy_moveit2/ros2_ws/src/my_moveit2drive/panda_moveit_config
+# Build directory: /home/yc-dlan/Dlancy_moveit2/ros2_ws/src/my_moveit2drive/panda_moveit_config/build/panda_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
